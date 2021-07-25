a9c16092183abde4c4e97e87283aafd2b98edb7a
TaiDang <tai.dt@kdc.vn>
2021-07-25 16:36:13 +0700

    Add google login on ios

--- src/App.js
+++ src/App.js
@@ -6,8 +6,25 @@ import AppContainer from './router';
 import {store} from './store';
 import './config/FoundationConfig';
 import './config/ComponentsConfig';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {useEffect} from 'react';
 
 export default App = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email', 'profile'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId:
+        '474017321493-s13l4f8om0bf499tmog71hd2d0juhu9s.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      hostedDomain: '', // specifies a hosted domain restriction
+      loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      accountName: '', // [Android] specifies an account name on the device that should be used
+      iosClientId:
+        '474017321493-s13l4f8om0bf499tmog71hd2d0juhu9s.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+      googleServicePlistPath: '', // [iOS] optional, if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+    });
+  }, []);
   return (
     <Provider store={store}>
       <AppContainer />
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,11 +4,30 @@ import {View, Text, Colors, Button, Image} from 'react-native-ui-lib';
 import {useNavigation} from '@react-navigation/native';
 import {Assets} from 'react-native-ui-lib';
 import {Dimensions} from 'react-native';
-import {ImageBackground} from 'react-native';
 const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 const LoginScreen = () => {
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
   const navigation = useNavigation();
   return (
     <View flex backgroundColor={Colors.loginBackgroundColor}>
@@ -44,6 +63,7 @@ const LoginScreen = () => {
 
           <View marginT-52 width={300} height={64}>
             <Button
+              onPress={signIn}
               size={1}
               enableShadow
               shadow
